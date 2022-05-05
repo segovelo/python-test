248ac19fb859a222bc614ec22a1285333125754b
segovelo <gauchosinpingo@gmail.com>
2022-05-05 22:43:39 +0100

    Changes to order list with least number of iterations

--- sort-list-test.py
+++ sort-list-test.py
@@ -5,36 +5,27 @@ Created on Thu Apr 28 22:03:31 2022
 @author: Sebastian
 """
 
-list1 = [9,0,4,0,1,0,0,0,0,7,0,0,5]
+list1 = [9,0,0,4,0,1,0,0,0,7,3,5]
 length = len(list1)
-c = 0
 i = 0
-zerosInRow = 0
-
+j = 0
+print(list1)
 while i < (length - 1):
-    c = c + 1
-    print('cycle : ' + str(c))
 
     if list1[i] == 0:
-        j = i - zerosInRow -1
-        zerosInRow = 0
-        print(' initial i ' + str(i))
-        print(' initial j ' + str(j))
+        i = i + 1
         while i < (length - 1) and (list1[i] == 0):
+            print(' Inside nested while loop : ' + str(i))
             i = i + 1
-            zerosInRow = zerosInRow + 1
-            print(' i inside while loop : '+ str(i))
-        print(' After while loop j  ' + str(j))
-        temp = list1[j]
-        print(' list1[i] : ' + str(list1[i]))
-        print('temp : ' + str(temp))
-       
+
+        temp = list1[j]       
         list1[j] = list1[i]
         list1[i] = temp
-        #i = j
-        print(list1)
-        print('====================================')
+        j = j + 1
+        
     else:
+        print(' Inside nested while loop : ' + str(i))
         i = i + 1
+        j = j + 1
 
 print(list1)
